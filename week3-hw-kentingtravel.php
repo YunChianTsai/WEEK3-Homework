fd51f14cd86329d47c4a77a4bb9a5cc84165e69e
YunChianTsai <100343318+YunChianTsai@users.noreply.github.com>
2022-03-16 20:58:05 +0800

    Add files via upload

---
+++ week3-hw-kentingtravel.php
@@ -0,0 +1,86 @@
+<html>
+<head>
+    <title>TRAVEL IN KENTING</title>
+    <link rel="icon" href="/img/earth.jpg" type="image/x-icon" />
+</head>
+
+<body>
+<?php
+echo "<body bgcolor='#cdb5cd'>";
+?>
+<div style="background-color:#eed2ee">
+    <center><h1><font color="68228b" size="8" face="圓體字體">墾丁三日遊</font></h1></center>
+</div>
+<h3><font color="483d8b" size="6" face="圓體字體">1.行程內容</font></h3>
+    <h1><font color="68228b" size="5" face="圓體字體">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp【鵝鑾鼻燈塔公園】&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+    【船帆石風景區】&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp【龍磐公園】</font></h1>
+    <a href="https://www.ktnp.gov.tw/News_Content2.aspx?n=5C28A0E32DF925F9&sms=C88B5251F308CE96&s=CC1B26C5C0691F1E"><img src='img/鵝鑾鼻燈塔公園.jpg' width='300' height='300'></a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+    <a href="https://www.dbnsa.gov.tw/Scenery-Content.aspx?lang=1&sno=04006749"><img src='img/船帆石風景區.jpg' width='300' height='300'></a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+    <a href="https://www.ktnp.gov.tw/News_Content2.aspx?n=28AB1D16ECF7E63C&sms=C88B5251F308CE96&s=C1E50972440D43B2"><img src='img/龍磐公園.jpg' width='300' height='300'></a>
+    <h1><font color="68228b" size="5" face="圓體字體">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp【白砂灣】
+    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp【九棚大沙漠】&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp【墾丁大街】</font></h1>
+    <p><a href="http://uukt.com.tw/attraction/21"><img src='img/白砂灣.jpg' width='300' height='300'></a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+    <a href="https://www.dbnsa.gov.tw/Scenery-Content.aspx?lang=1&sno=04006742"><img src='img/九棚大沙漠.jpg' width='300' height='300'></a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+    <a href="https://www.travelking.com.tw/tourguide/scenery104822.html"><img src='img/墾丁大街.jpg' width='300' height='300'></a>
+
+<h3><font color="483d8b" size="6" face="圓體字體">2.報名表單</font></h3>
+
+<form action="week3-hw-thank.php" method="post">
+NAME：<input type="text" name="name" required>
+<p>EMAIL：<input type="email" name="email" required>
+<p>PHONE NUMBER：<input type="tel" name="phonrnumber" required>
+<P>BIRTHDAY：<input type="date" name="birthday" required>
+<p>GENDER：<input type="radio" name="gender">MALE<input type="radio" name="gender">FEMALE
+<p>FOOD PREFERENCE：<input type="checkbox" name="food">雞腿便當<input type="checkbox" name="food">排骨便當<input type="checkbox" name="food">魚排便當<input type="checkbox" name="food">素食便當
+<p>
+T-SHIRT SIZE：
+<select name="size">
+<option>XS</option>
+<option>S</option>
+<option>M</option>
+<option>L</option>
+<option>XL</option>
+</select>
+<p>
+T-SHIRT COLOR：
+<select name="color">
+<option>象牙白</option>
+<option>石墨黑</option>
+<option>天峰藍</option>
+<option>松嶺青</option>
+<option>胭脂粉</option>
+</select>
+<p>TICKET：<input type="number" name="ticket" required>
+
+<p>COMMENT：</br>
+<textarea name="comment" cols="40" rows="10">
+</textarea>
+
+<P><input type="submit">
+</form>
+
+<h3><font color="483d8b" size="6" face="圓體字體">3.衣服尺寸表</font></h3>
+<center>
+<div style="width:500px;height:318px;background-color:#eed2ee">
+<table style="border:3px #68228b double" cellpadding="10" width="500" border="5">
+<tr><td colspan="5"><font color="191970" size="5" face="圓體字體"><center><b>SIZE TABLE</b></center></font></tr>   
+<tr><th><font color="000080" size="4" face="圓體字體">尺碼</font></th><th><font color="000080" size="4" face="圓體字體">衣長</font></th><th><font color="000080" size="4" face="圓體字體">胸圍</font></th><th><font color="000080" size="4" face="圓體字體">肩寬</font></th><th><font color="000080" size="4" face="圓體字體">袖長</font></th></tr>
+<tr><th><font color="000080" size="2" face="圓體字體">XS</font></th><th>64</th><th>100</th><th>41.5</th><th>21</th></tr>
+<tr><th><font color="000080" size="2" face="圓體字體">S</font></th><th>65</th><th>104</th><th>44</th><th>22</th></tr>
+<tr><th><font color="000080" size="2" face="圓體字體">M</font></th><th>66</th><th>108</th><th>46.5</th><th>23</th></tr>
+<tr><th><font color="000080" size="2" face="圓體字體">L</font></th><th>67</th><th>112</th><th>49</th><th>24</th></tr>
+<tr><th><font color="000080" size="2" face="圓體字體">XL</font></th><th>68</th><th>116</th><th>51.5</th><th>25</th></tr>
+</div>
+</table>
+</center>
+<div style="background-color:#eed2ee">
+    <center><h1><font color="68228b" size="5" face="圓體字體">網頁程式設計 WEEK3 HOMEWORK A1074146 應用數學系 蔡昀蒨</font></h1></center>
+</div>
+
+</body>
+</html>
